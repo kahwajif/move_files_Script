7b93db04ba9fc8ff0fcdf15a23f5bb2dae67744d
Feras Kahwaji <feraskahwaji@hotmail.com>
2021-02-28 04:00:40 -0500

    Fixed file download error

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # move_files_Script
-A script that moves downloaded files from my Downloads folder to their respective designated folder
+A script for Windows that moves downloaded files from my Downloads folder to their respective designated folder. 
+
+Example: MP3 files -> Music Folder, PNG and JPEG -> Pictures Folder, MP4 -> Video Folders
+Files with already existing names will have a random number generated and appended to them.
--- move_files_script.py
+++ move_files_script.py
@@ -11,7 +11,7 @@ music_dir = "C:/Users/feras/Music"
 documents_dir = "C:/Users/feras/OneDrive/Documents"
 
 #file extensions
-pic_ext = ".png", ".jpg", ".jpeg", ".gif", ".bmp", ".svg", ".psd"
+pic_ext = ".png", ".jpg", ".jpeg", ".gif", ".bmp", ".svg", ".psd", ".PNG"
 vid_ext = ".wmv", ".amv", ".mp4", ".mov", ".flv", ".webm", ".mkv"
 mus_ext = ".wav", ".aa", ".aiff", ".alac", ".m4p", ".mp3", ".wma"
 doc_ext = ".pdf", ".doc", ".docx", ".xlsx", ".xls", ".ppt", ".pptx", ".txt", ".html"
@@ -20,31 +20,34 @@ doc_ext = ".pdf", ".doc", ".docx", ".xlsx", ".xls", ".ppt", ".pptx", ".txt", ".h
 class Handler(FileSystemEventHandler):
     def on_modified(self, event):
         for file_name in os.listdir(download_dir):
+            time.sleep(0.25)#without this time delay, downloaded files wont save
             #move pictures
             if any(ext in file_name for ext in pic_ext):
                 new_name = self.file_exists(picture_dir,file_name)
                 src = download_dir + "/" + file_name
                 os.rename(src, picture_dir + "/" + new_name)
+            
             #move videos
             elif any(ext in file_name for ext in vid_ext):
                 new_name = self.file_exists(video_dir,file_name)
                 src = download_dir + "/" + file_name
-                os.rename(src, video_dir + "/" + file_name)
+                os.rename(src, video_dir + "/" + new_name)
             #move music    
             elif any(ext in file_name for ext in mus_ext):
                 new_name = self.file_exists(music_dir,file_name)
                 src = download_dir + "/" + file_name
-                os.rename(src, music_dir + "/" + file_name)
-            #move documents
+                os.rename(src, music_dir + "/" + new_name)
+            
             '''
+            #move documents
             elif any(ext in file_name for ext in doc_ext):
                 src = download_dir + "/" + file_name
                 os.rename(src, documents_dir + "/" + file_name)
             '''
 
     #if file name exists, generate random number & add before file name
-    def file_exists(self, dir, file_name):
-        file_exists = os.path.isfile(dir + "/" + file_name)
+    def file_exists(self, direct, file_name):
+        file_exists = os.path.isfile(direct + "/" + file_name)
         if file_exists:
             new_name = str(random.randrange(100)) + "_" + file_name
             return new_name
@@ -57,8 +60,8 @@ observer.schedule(event_handler, download_dir, recursive = True)
 observer.start()
 
 try:
-    while True:
-        time.sleep(10)
+   while True:
+            time.sleep(5)
 except KeyboardInterrupt:
     observer.stop()
 
